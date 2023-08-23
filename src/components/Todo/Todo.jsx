53635820ee3fecba36c291b4c7c9ff1878664776
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-08-23 22:38:32 +0600

    added all the functionality to the todo component

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,8 +1,28 @@
+import { useDispatch } from 'react-redux';
 import cancelPng from '../../assets/images/cancel.png';
+import {
+	colorSelected,
+	deleted,
+	toggled,
+} from '../../redux/todos/actionCreators';
 
 export default function Todo({ todo }) {
+	const dispatch = useDispatch();
+
 	const { text, completed, priority, id } = todo;
-	console.log(priority);
+
+	const handleToggleStatus = (id) => {
+		dispatch(toggled(id));
+	};
+
+	const handleChangeColor = (id, color) => {
+		dispatch(colorSelected(id, color));
+	};
+
+	const handleDeleteTodo = (id) => {
+		dispatch(deleted(id));
+	};
+
 	return (
 		<div className='flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0'>
 			<div
@@ -14,7 +34,8 @@ export default function Todo({ todo }) {
 				<input
 					type='checkbox'
 					checked={completed}
-					className='opacity-0 absolute rounded-full'
+					className='opacity-0 absolute rounded-full cursor-pointer'
+					onChange={() => handleToggleStatus(id)}
 				/>
 				<svg
 					className={`fill-current w-3 h-3 text-green-500 pointer-events-none ${
@@ -33,22 +54,26 @@ export default function Todo({ todo }) {
 			<div
 				className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 ${
 					priority === 'green' && 'bg-green-500'
-				}`}></div>
+				}`}
+				onClick={() => handleChangeColor(id, 'green')}></div>
 
 			<div
 				className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500 ${
 					priority === 'yellow' && 'bg-yellow-500'
-				}`}></div>
+				}`}
+				onClick={() => handleChangeColor(id, 'yellow')}></div>
 
 			<div
 				className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500 ${
 					priority === 'red' && 'bg-red-500'
-				}`}></div>
+				}`}
+				onClick={() => handleChangeColor(id, 'red')}></div>
 
 			<img
 				src={cancelPng}
 				className='flex-shrink-0 w-4 h-4 ml-2 cursor-pointer'
 				alt='Cancel'
+				onClick={() => handleDeleteTodo(id)}
 			/>
 		</div>
 	);
