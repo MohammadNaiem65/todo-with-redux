a520cd4d0fb25b783616d306b6ebca61f9c7bf3c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-08-24 11:38:24 +0600

    added functionality of showing todo list depending on filter

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,11 +3,44 @@ import Todo from '../Todo/Todo';
 
 export default function TodoList() {
 	const todos = useSelector((state) => state.todos);
+	const filter = useSelector((state) => state.filter);
+
+	const filterByStatus = (todo) => {
+		switch (filter.status) {
+			case 'complete': {
+				return todo.completed;
+			}
+
+			case 'incomplete': {
+				return !todo.completed;
+			}
+
+			default: {
+				return true;
+			}
+		}
+	};
+
+	const filterByColors = (todo) => {
+		if (filter.colors.length > 0) {
+			if (filter.colors.includes(todo?.priority)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return true;
+		}
+	};
+
 	return (
 		<div className='mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto'>
-			{todos.map((todo) => (
-				<Todo key={todo.id} todo={todo} />
-			))}
+			{todos
+				.filter(filterByStatus)
+				.filter(filterByColors)
+				.map((todo) => (
+					<Todo key={todo.id} todo={todo} />
+				))}
 		</div>
 	);
 }
